91b38448ddc34c242105fc1bbc72dec47dcf972d
Blake Thompson <blakeathompson94@gmail.com>
2021-06-21 17:12:33 -0500

    api is up and running, need to figure out how to deal with connecting flights

--- src/components/SearchFlight.js
+++ src/components/SearchFlight.js
@@ -11,30 +11,35 @@ export default function SearchFlight() {
     const [flightScheduledArrival, setFlightScheduledArrival] = useState('');
     const [flightActualArrival, setFlightActualArrival] = useState('');
     const [flightScheduledDeparture, setFlightScheduledDeparture] = useState('');
-    const [flightNumber, setFlightNumber] = useState('');
+    const [flightNumberId, setFlightNumberId] = useState('');
 
-    const handleInputChange = (event) => {
-      event.preventDefault();
-      setFlightNumber({
-          ...flightNumber,
-          [event.target.name]: event.target.value,
-      }); 
-    };
+    // const handleInputChange = (event) => {
+    //   event.preventDefault();
+    //   setFlightNumberId({
+    //       ...flightNumberId,
+    //       [event.target.name]: event.target.value,
+    //   }); 
+    // };
     
     const handleSubmit = async (event) => {
         event.preventDefault();
+        console.log(flightNumberId);
         const inputFlightNumber = {
-            flashFlightNumber: flightNumber.flightNumber 
+            flashFlightNumber: flightNumberId.flightNumber 
         }
-        setFlightNumber(inputFlightNumber);
-        afterSubmit();
+        setFlightNumberId(inputFlightNumber);
+        afterSubmit(flightNumberId);
     }
 
     const afterSubmit = async () => {
-        const flightNumber = {
-            findFlight: flightNumber.inputFlightNumber
-        }
-        await flightTracker.getFlashFlight(flightNumber);
+        // const flightNumber = {
+        //     findFlight: flightNumberId
+        // }
+        
+        await flightTracker.getFlashFlight(flightNumberId).then((response) => {
+            setFlightNumberId()
+            console.log(response);
+        });
     }
    
 
@@ -46,13 +51,14 @@ export default function SearchFlight() {
         className="flashFlightForm" 
         size="lg" type="text"  
         placeholder="Flight Number"
-        name='flightNumber' 
-        handleInputChange={handleInputChange}/>
+        name='flightNumberId'
+        value={flightNumberId}
+        onChange={(e) => setFlightNumberId(e.target.value)}/>
         </Col>
         </Row>
         <Row>
         <Col xs={12} s={10} m={8} l={6} md={{span: 6, offset: 5}}>
-        <Button size="lg" variant="dark" handleSubmit={handleSubmit} type="submit" className="flightSubmit" on>
+        <Button size="lg" variant="dark" onClick={handleSubmit} type="submit" className="flightSubmit" on>
         The Fullest Send <FaFighterJet />
         </Button>
         </Col>  
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,23 +1,19 @@
 import axios from 'axios';
 require('dotenv').config();
 
+
 export const flightTracker = {
     async getFlashFlight(flightNumber) {
         let flashFlight;
         await axios({
             method: 'GET',
-            url: 'https://aerodatabox.p.rapidapi.com/flights/number/',
+            url: `https://aerodatabox.p.rapidapi.com/flights/number/WN${flightNumber}`,
             headers: {
-                "headers": {
-                    "x-rapidapi-key": process.env.apiKey,
-                    "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
-                    useQueryString: true,
-
-                }
+                 "x-rapidapi-key": "74b3760cb2msh8f443e6234efc8ep1c1ef6jsn4ecfc3d16958",
+                 "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+                 useQueryString: true,
             },
-            params: {
-                input: `${flightNumber}`,
-            },
+            
         })
         .then((response) => {
             flashFlight = response;

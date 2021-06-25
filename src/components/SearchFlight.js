31bbb36640177cfa347c550f3b29afc03ec531c7
Blake Thompson <blakeathompson94@gmail.com>
2021-06-25 16:48:41 -0500

    messed up somewhere, cant render response data into table body. Time to dig

--- src/components/SearchFlight.js
+++ src/components/SearchFlight.js
@@ -1,4 +1,4 @@
-
+import moment from 'moment';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,11 +7,21 @@ import  { FaFighterJet }  from 'react-icons/fa';
 import { useState } from 'react';
 import { flightTracker } from '../utils/API';
 
+
 export default function SearchFlight() {
     const [flightScheduledArrival, setFlightScheduledArrival] = useState('');
     const [flightActualArrival, setFlightActualArrival] = useState('');
     const [flightScheduledDeparture, setFlightScheduledDeparture] = useState('');
+    const [flightNumber, setFlightNumber] = useState('');
     const [flightNumberId, setFlightNumberId] = useState('');
+        
+    const emptyFlight = {
+        flightNumber : '',
+        flightScheduledDeparture : '',
+        flightScheduledArrival : '',
+    }
+    const [userFlight, setUserFlight]= useState(emptyFlight);
+
 
     // const handleInputChange = (event) => {
     //   event.preventDefault();
@@ -35,12 +45,40 @@ export default function SearchFlight() {
         // const flightNumber = {
         //     findFlight: flightNumberId
         // }
-        
+        // const timeSetup = moment().format('LLLL');
+
         await flightTracker.getFlashFlight(flightNumberId).then((response) => {
-            setFlightNumberId()
             console.log(response);
-        });
-    }
+            let flightNumb = response.data[0].number;
+            console.log(flightNumb);
+            setFlightNumber(flightNumb);
+            let scheduledArrival =  response.data[0].arrival.scheduledTimeLocal;
+            // console.log(moment(`${scheduledArrival}`).format('LLLL'));
+            // moment(`${scheduledArrival}`).format('LLLL') = scheduledArrival;
+            scheduledArrival = moment(`${scheduledArrival}`).format('LLLL');
+            let scheduledDeparture = response.data[0].departure.scheduledTimeLocal;
+            // console.log(moment(`${scheduledDeparture}`).format('LLLL'));
+            scheduledDeparture = moment(`${scheduledDeparture}`).format('LLLL') 
+            console.log(flightNumb, scheduledDeparture, scheduledArrival);
+            setFlightNumber(flightNumb);
+            setFlightScheduledDeparture(scheduledDeparture);
+            setFlightScheduledArrival(scheduledArrival);
+            // console.log(flightNumber,flightScheduledDeparture,flightScheduledArrival);
+            setUserFlight(flightNumber.flightNumb,flightScheduledDeparture.scheduledDeparture,flightScheduledArrival.scheduledArrival);
+            console.log(userFlight);
+        //     Body.append(    
+        //         <tbody id='scheduledTimes'>
+        //             <tr>
+        //             <td>
+        //               {flightNumber.flightNumb}{flightScheduledDeparture.scheduledDeparture}{flightScheduledArrival.scheduledArrival}  
+        //             </td>
+        //             </tr>
+        //         </tbody>
+                
+            
+        // )
+    });
+};
    
 
     return (
@@ -51,9 +89,9 @@ export default function SearchFlight() {
         className="flashFlightForm" 
         size="lg" type="text"  
         placeholder="Flight Number"
-        name='flightNumberId'
+        onChange={(e) => setFlightNumberId(e.target.value)}
         value={flightNumberId}
-        onChange={(e) => setFlightNumberId(e.target.value)}/>
+        />
         </Col>
         </Row>
         <Row>
@@ -63,8 +101,6 @@ export default function SearchFlight() {
         </Button>
         </Col>  
         </Row>
-     
-  
     </Form.Group>
-     )
+    )
 }
